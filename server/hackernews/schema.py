b6275599e5404a69014be23000bff0440ba63c18
Jieming Liu <jliu@exscientia.com>
2020-05-25 01:09:06 +0100

    first commit
    Upload files

---
+++ server/hackernews/schema.py
@@ -0,0 +1,29 @@
+import graphene
+import graphql_jwt
+import users.schema
+import links.schema
+import links.schema_relay
+
+
+class Query(
+    users.schema.Query,
+    links.schema.Query,
+    links.schema_relay.RelayQuery,
+    graphene.ObjectType,
+):
+    pass
+
+
+class Mutation(
+    users.schema.Mutation,
+    links.schema.Mutation,
+    links.schema_relay.RelayMutation,
+    graphene.ObjectType,
+):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
